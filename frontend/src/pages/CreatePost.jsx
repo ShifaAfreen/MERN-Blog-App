6179c64c97f6f29b11f2ecc5923017108094395d
ShifaAfreen <92392426+ShifaAfreen@users.noreply.github.com>
2024-07-07 13:32:42 +0530

    Update CreatePost.jsx

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -11,14 +11,18 @@ export default function CreatePost(){
     const[content, setContent] = useState('');
     const[files, setFiles] = useState('');
     const[redirect, setRedirect] = useState(false);
+    const [errorMessage, setErrorMessage] =  useState('');
+
     
       async function createNewPost(ev){
+
         const data = new FormData();
         data.set('title', title);
         data.set('summary', summary);
         data.set('content', content);
         data.set('file',files[0]);
 
+        setErrorMessage('');
         ev.preventDefault();
         const response = await fetch('https://mern-blog-app-backend-aaqs.onrender.com/post',{
             method: 'POST',
@@ -28,6 +32,10 @@ export default function CreatePost(){
 
         if(response.ok){
           setRedirect(true);
+        }else {
+            console.error('Failed to create post');
+            const errorData = await response.json();
+            setErrorMessage(errorData.error);
         }
       }
 
@@ -37,6 +45,7 @@ export default function CreatePost(){
 
     return(
         <form onSubmit={createNewPost}>
+          {errorMessage && <div style={{color:'red'}}>{errorMessage}</div>}
             <input type="title" 
                     placeholder={'Title'}
                     value={title}
@@ -48,7 +57,9 @@ export default function CreatePost(){
             <input type="file"
                     onChange={ev=> setFiles(ev.target.files)} />
             <Editor value={content} onChange={setContent}/>
+            <div className="button-container">
             <button style={{marginTop: '5px'}}> Create Post</button>
+            </div>
 
         </form>
     );
